e9675aa11aa14c324b9d140f045dcbe6d7f35ef3
liwen <499014099@qq.com>
2020-06-03 13:43:36 +0800

    初始化0.1

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 # 构建新的镜像  注意 .  不能少。
-sudo  docker build -t registry.cn-hangzhou.aliyuncs.com/atliwen/elemenet:0.0.10 .;
+docker build -t registry.cn-hangzhou.aliyuncs.com/atliwen/elemenet:0.0.10 .;
 # 登录 私有镜像仓库
 #sudo  docker login  -u 用户名 -p 密码 hbt.zjs.com.cn;
-sudo docker login --username=18210051699 -p qq123123 registry.cn-hangzhou.aliyuncs.com
+docker login --username=18210051699 -p qq123123 registry.cn-hangzhou.aliyuncs.com
 # 推送本地镜像到镜像厂库
 # sudo  docker push 系统名称:版本号;
 
-sudo docker push registry.cn-hangzhou.aliyuncs.com/atliwen/elemenet:0.0.10
+docker push registry.cn-hangzhou.aliyuncs.com/atliwen/elemenet:0.0.10
